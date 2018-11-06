3337ccdfb4c0398bc1be9df7b9a3be1cadce2306
kiting'u <benlegendj@localhost.localdomain>
2018-11-06 09:35:27 +0300

    [finishes 161742894] configured running environment

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from instance.config import app_config
 
 
-def create_app():
-    app=Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
     from .api.v1 import v1_blueprint
 
-    app.register_blueprint(v1_blueprint,url_prefix='/api/v1')
-
+    app.register_blueprint(v1_blueprint, url_prefix='/api/v1')
+    app.url_map.strict_slashes = False
     return app
