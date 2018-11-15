a82c60fbbe2db81b9d64ac562577b76b34fea7ae
kiting'u <benlegendj@localhost.localdomain>
2018-11-15 08:33:07 +0300

    [finishes #161958259] improved user'sinput validation

--- app/api/utils/parcel_validator.py
+++ app/api/utils/parcel_validator.py
@@ -1,17 +1,20 @@
 from marshmallow import Schema, fields, ValidationError
 from flask_restplus import reqparse, inputs
-
+import re
 
 def validate_length(input):
     if input.strip() == '':
         raise ValidationError('fields cannot be blank')
-    elif len(input) < 3:
+    elif not re.match(r"^(?=.*[a-z])[a-zA-Z0-9_.-]{3,25}$", input):
         raise ValidationError("{} is not a valid input".format(input))
-
 
 def validate_weight(weight):
     if weight < 0:
         raise ValidationError("please provide valid weight")
+
+def validate_password(password):
+    if not re.match(r'[A-Za-z0-9_@#$%^&\*+=,\.`\-]{6,}',password):
+        raise ValidationError("please should provide a strong password")
 
 
 class ParcelSchema(Schema):
@@ -25,8 +28,8 @@ class ParcelSchema(Schema):
 
 class UserSchema(Schema):
     username = fields.String(required=True, validate=validate_length)
-    email = fields.Email(required=True, validate=validate_length)
-    password = fields.String(required=True, validate=validate_length)
+    email = fields.Email(required=True)
+    password = fields.String(required=True, validate=validate_password)
 
 
 class LoginParser:
@@ -42,7 +45,7 @@ class LoginParser:
                         type=inputs.regex(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'),
                         required=True,
                         location='json',
-                        help="please enter a valid date")
+                        help="please enter a valid email")
 
 
 class DestinationParser:
