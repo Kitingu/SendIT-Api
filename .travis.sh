05bd4e7bed7b0b79c2c6c0ecc44a80967cc44eeb
kiting'u <benlegendj@localhost.localdomain>
2018-11-20 17:17:36 +0300

    [delivers #162031629] add travis environment variables

---
+++ .travis.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export DATABASE_URI="dbname= 'postgres' user='postgres' password='' host='localhost'"
+export TESTDB_URI="dbname= 'test_db' user='postgres' password='' host='localhost'"
+export JWT_SECRET_KEY="this is not secret"
